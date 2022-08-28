28f5941bade1673ea63bb61a2c87164246f055fd
roman.lysenko <roman.lysenko@axel.com>
2022-08-29 02:09:21 +0300

    Added swagger and make test-function in User.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserService } from './user.service';
 
+@ApiTags('User')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  
+  @Get('/getHello')
+  getHello(): Promise<string> {
+    return this.userService.getHello();
+  }
+}
